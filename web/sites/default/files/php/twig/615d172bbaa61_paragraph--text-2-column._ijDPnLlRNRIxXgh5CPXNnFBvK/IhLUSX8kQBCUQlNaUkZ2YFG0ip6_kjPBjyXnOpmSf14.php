c87b447228e459608bb23e31b4caccc309c4c071
Jenny Lee <jennbina@mit.edu>
2021-10-06 18:38:59 -0700

    committing all the rest

---
+++ web/sites/default/files/php/twig/615d172bbaa61_paragraph--text-2-column._ijDPnLlRNRIxXgh5CPXNnFBvK/IhLUSX8kQBCUQlNaUkZ2YFG0ip6_kjPBjyXnOpmSf14.php
@@ -0,0 +1,187 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/jennytheme/templates/paragraphs/paragraph--text-2-column.html.twig */
+class __TwigTemplate_950fb21c82d734ed565462dbed98f7f813ddb1d060b729b6eba6c253494e47a1 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'paragraph' => [$this, 'block_paragraph'],
+            'content' => [$this, 'block_content'],
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 42
+        $context["classes"] = [0 => "row", 1 => "paragraph", 2 => ("paragraph--type--" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source,         // line 44
+($context["paragraph"] ?? null), "bundle", [], "any", false, false, true, 44), 44, $this->source))), 3 => ((        // line 45
+($context["view_mode"] ?? null)) ? (("paragraph--view-mode--" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["view_mode"] ?? null), 45, $this->source)))) : (""))];
+        // line 48
+        $this->displayBlock('paragraph', $context, $blocks);
+    }
+
+    public function block_paragraph($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 49
+        echo "\t";
+        $this->displayBlock('content', $context, $blocks);
+    }
+
+    public function block_content($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 50
+        echo "\t\t<div";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 50), 50, $this->source), "html", null, true);
+        echo ">
+\t\t\t<div class=\"col-sm-6\">
+\t\t\t\t";
+        // line 52
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_content_left_column", [], "any", false, false, true, 52), 52, $this->source), "html", null, true);
+        echo "
+\t\t\t</div>
+\t\t\t<div class=\"col-sm-6\">
+\t\t\t\t";
+        // line 55
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_content_right_column", [], "any", false, false, true, 55), 55, $this->source), "html", null, true);
+        echo "
+\t\t\t</div>
+\t\t</div>
+
+\t";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/jennytheme/templates/paragraphs/paragraph--text-2-column.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  72 => 55,  66 => 52,  60 => 50,  52 => 49,  45 => 48,  43 => 45,  42 => 44,  41 => 42,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Default theme implementation to display a paragraph.
+ *
+ * Available variables:
+ * - paragraph: Full paragraph entity.
+ *   Only method names starting with \"get\", \"has\", or \"is\" and a few common
+ *   methods such as \"id\", \"label\", and \"bundle\" are available. For example:
+ *   - paragraph.getCreatedTime() will return the paragraph creation timestamp.
+ *   - paragraph.id(): The paragraph ID.
+ *   - paragraph.bundle(): The type of the paragraph, for example, \"image\" or \"text\".
+ *   - paragraph.getOwnerId(): The user ID of the paragraph author.
+ *   See Drupal\\paragraphs\\Entity\\Paragraph for a full list of public properties
+ *   and methods for the paragraph object.
+ * - content: All paragraph items. Use {{ content }} to print them all,
+ *   or print a subset such as {{ content.field_example }}. Use
+ *   {{ content|without('field_example') }} to temporarily suppress the printing
+ *   of a given child element.
+ * - attributes: HTML attributes for the containing element.
+ *   The attributes.class element may contain one or more of the following
+ *   classes:
+ *   - paragraphs: The current template type (also known as a \"theming hook\").
+ *   - paragraphs--type-[type]: The current paragraphs type. For example, if the paragraph is an
+ *     \"Image\" it would result in \"paragraphs--type--image\". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - paragraphs--view-mode--[view_mode]: The View Mode of the paragraph; for example, a
+ *     preview would result in: \"paragraphs--view-mode--preview\", and
+ *     default: \"paragraphs--view-mode--default\".
+ * - view_mode: View mode; for example, \"preview\" or \"full\".
+ * - logged_in: Flag for authenticated user status. Will be true when the
+ *   current user is a logged-in member.
+ * - is_admin: Flag for admin user status. Will be true when the current user
+ *   is an administrator.
+ *
+ * @see template_preprocess_paragraph()
+ *
+ * @ingroup themeable
+ */
+#}
+{%
+  set classes = ['row',
+    'paragraph',
+    'paragraph--type--' ~ paragraph.bundle|clean_class,
+    view_mode ? 'paragraph--view-mode--' ~ view_mode|clean_class
+  ]
+%}
+{% block paragraph %}
+\t{% block content %}
+\t\t<div{{attributes.addClass(classes)}}>
+\t\t\t<div class=\"col-sm-6\">
+\t\t\t\t{{ content.field_content_left_column }}
+\t\t\t</div>
+\t\t\t<div class=\"col-sm-6\">
+\t\t\t\t{{ content.field_content_right_column }}
+\t\t\t</div>
+\t\t</div>
+
+\t{% endblock %}
+{% endblock paragraph %}
+", "themes/custom/jennytheme/templates/paragraphs/paragraph--text-2-column.html.twig", "C:\\xampp\\htdocs\\recipe3\\web\\themes\\custom\\jennytheme\\templates\\paragraphs\\paragraph--text-2-column.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 42, "block" => 48);
+        static $filters = array("clean_class" => 44, "escape" => 50);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'block'],
+                ['clean_class', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
